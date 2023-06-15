938631940f1aa3a6b527b6193726c0a201a998bf
Ttigone <2523849921@qq.com>
2023-06-15 09:44:25 +0800

    fsession

--- App.cpp
+++ App.cpp
@@ -1,13 +1,15 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
-
+#include <QIcon>
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
 
+    app.setWindowIcon(QIcon(":/images/music"));
+
     QQmlApplicationEngine engine;
-    const QUrl url(u"qrc:/Qtmusic_video/main.qml"_qs);
+    const QUrl url(u"qrc:/Qtmusic_video/App.qml"_qs);
     QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
                      &app, [url](QObject *obj, const QUrl &objUrl) {
         if (!obj && url == objUrl)
--- App.qml
+++ App.qml
@@ -1,8 +1,9 @@
 import QtQuick
-import QtQuick.Window 2.12
+
+
 Window {
     width: 1200
     height: 800
     visible: true
-    title: qsTr("Hello World")
+    title: qsTr("music video")
 }
